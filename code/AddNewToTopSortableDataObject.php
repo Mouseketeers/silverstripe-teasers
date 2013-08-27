f236f5a75047637950760cc03da00821df505f57
Henrik Olsen <henrik@mouseketeers.dk>
2013-08-27 10:00:35 +0200

    Fixed add to top feature

--- code/AddNewToTopSortableDataObject.php
+++ code/AddNewToTopSortableDataObject.php
@@ -3,10 +3,15 @@ class AddNewToTopSortableDataObject extends SortableDataObject {
 	public function onBeforeWrite() {
 		if(!$this->owner->ID) {
 			if($peers = DataObject::get($this->owner->class)) {
-				$this->owner->SortOrder = 0;
-				foreach($peers as $peer) {
-					$peer->SortOrder++;
-					$peer->write(); 
+				if($this->owner->class == 'Teaser') {
+					$this->owner->SortOrder = 0;
+					foreach($peers as $peer) {
+						$peer->SortOrder++;
+						$peer->write(); 
+					}					
+				}
+				else {
+					$this->owner->SortOrder = $peers->Count()+1;
 				}
 			}
 		}
