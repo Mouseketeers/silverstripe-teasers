f5af2c06642c61d64aed36ab6830f529b91c63d3
Henrik Olsen <henrik@mouseketeers.dk>
2015-08-03 11:39:08 +0200

    Updated to support SilverStripe 3.1

--- code/Teaser.php
+++ code/Teaser.php
@@ -7,6 +7,8 @@ class Teaser extends DataObject {
 		'LinkTitle' => 'Varchar(255)',
 		'Global' => 'Boolean',
 		'ColumnWidth' => 'Varchar',
+		'Active' => "Boolean",
+		"SortOrder" => "Int"
 	);
 	static $has_one = array (
 		'Image' => 'Image',
@@ -15,12 +17,29 @@ class Teaser extends DataObject {
 	static $belongs_many_many = array(
 		'Pages' => 'Page'
 	);
+	static $default_sort = "SortOrder";
+	static $field_labels = array(
+		'Active.Nice' => 'Active'
+	);
+
+	static $summary_fields = array(
+		"Thumbnail",
+		"Title",
+		"Active.Nice"
+	);
+	private static $defaults = array(
+		'Active' => true
+	);
 	public function populateDefaults() {
 		$this->ColumnWidth = $this->getDefaultColumnWidth();
 		parent::populateDefaults();
 	}
-	function getThumbnailOfTeaserImage() {
-		return $this->Image()->StripThumbnail();
+	function getThumbnail() {
+		if (((int) $this->ImageID > 0) && (is_a($this->Image(),'Image')))  {
+			return $this->Image()->SetWidth(50);
+		} else {
+			return 'No image';
+		}
 	}
 	function getContentSummary() {
 		return $this->dbObject('Content')->LimitCharacters(80);
@@ -29,6 +48,7 @@ class Teaser extends DataObject {
 		return $this->Global ? 'Shown on all pages' : '';
 	}
 	function ColumnWidth() {
+		return $this->getDefaultColumnWidth();
 		$column_width = $this->dbObject('ColumnWidth')->value;
 		if(!$column_width) {
 			$column_width = $this->getDefaultColumnWidth();
@@ -39,36 +59,37 @@ class Teaser extends DataObject {
 		$default_column_width = Teasers::$default_column_width;
 		// default to first item in array if no default column width is specified
 		if(!$default_column_width) {
-			reset(Teasers::$column_widths);
-			$default_column_width = key(Teasers::$column_widths);
+			// debug::dump(end(Teasers::$column_widths));
+			$default_column_width = end(Teasers::$column_widths);
+			// reset(Teasers::$column_widths);
+			// $default_column_width = key(Teasers::$column_widths);
 		}
 		return $default_column_width;
-	} 
-	function getCMSFields_forPopup() {
-		$fields = new FieldSet();
-		$fields->push(new TextField('Title', 'Title'));
-		$fields->push(new FileUploadField('Image', 'Image'));
-		$fields->push(new SimpleTinyMCEField('Content','Content'));
-		$PageDropDown = new SimpleTreeDropdownField('LinkID', 'Link to page');
-		$PageDropDown->setEmptyString('-- None --');
-		$fields->push($PageDropDown);
-		$fields->push(new TextField('ExternalLink', 'Link to another website'));
-		$fields->push(new TextField('LinkTitle', 'Link label'));
-		if(Teasers::$enable_column_width) {
-			$fields->push( new DropdownField(
-					$name = 'ColumnWidth',
-					$title = _t('Teaser.COLUMNS','Width of teaser (in columns)'),
-					$options = Teasers::$column_widths,
-					$value = null,
-					$form = null,
-					$emptyString = 'Default'
-				)
-			);
-		}
+	}
+	public function getCMSFields(){
+		$fields = new FieldList();
+		$fields->push(
+			new TabSet('Root', 
+				new Tab('Main', 'Main')
+			)
+		);
+		$fields->addFieldsToTab('Root.Main',
+			array(
+				TextField::create('Title'),
+				UploadField::create('Image')
+					->setTitle('Image')
+					->setFolderName('Teasers')
+					->setConfig('allowedMaxFileNumber', 1),
+				TreeDropdownField::create('LinkID', 'Link to internal page', 'SiteTree'),
+				TextField::create('ExternalLink', 'Link to external page')
+					->setDescription('Include http:// (e.g. http://www.example.com)'),
+				HTMLEditorField::create('Content'),
+				CheckboxField::create("Active")->setTitle("Active")
+			)
+		);
 		if(Teasers::$enable_global_teasers) {
 			$fields->push(new CheckboxField('Global', 'Show this teaser on all pages'));
 		}
-		$fields->push( new LiteralField('DOM-fix','<div style="height:35px">&nbsp;</div>'));
 		return $fields;
 	}
 }
---
+++ templates/Includes/FlexWidthTeasers.ss
@@ -0,0 +1,21 @@
+<% if ManagedTeasers %>
+<div class="teasers row">
+	<% loop ManagedTeasers %>
+	<div class="teaser large-$ColumnWidth columns <% if Last %> end<% end_if %>">
+			<% if Title %><h3>$Title</h3><% end_if %>
+			<% if Image %>$Image.SetWidth(300)<% end_if %>
+			$Content
+			<p>
+			<% if Link || ExternalLink %>
+				<a href="<% if Link %>$Link.URLSegment<% else_if ExternalLink %>$ExternalLink<% end_if %>">
+					<% if LinkTitle %>$LinkTitle<% else %>Read more<% end_if %>
+				</a>
+			<% end_if %>
+			</p>
+	</div>
+	<% end_loop %>
+</div>
+<% end_if %>
+
+
+	
